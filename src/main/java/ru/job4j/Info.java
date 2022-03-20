d3f213170a6335d0f31848c1483eb50651c1ff9a
redares <lodroft@gmail.com>
2022-03-20 17:54:05 +0300

    6. Checkstyle [#4123 #22338]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,8 +1,7 @@
 package ru.job4j;
 
-public class Main {
+public class Info {
     public static void main(String[] args) {
-
-        System.out.println("Hello world");
+        System.out.println("20.03.2022");
     }
 }
